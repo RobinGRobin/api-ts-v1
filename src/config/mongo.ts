833c8f7af12b8a7a2a4de708d2c23e059ac9da4c
Robin <robin@MacBook-de-Robin.local>
2023-07-06 11:59:08 -0600

    mongo uri config changed

--- src/config/mongo.ts
+++ src/config/mongo.ts
@@ -2,7 +2,8 @@ import "dotenv/config";
 import { connect } from "mongoose";
 
 async function dbConnect(): Promise<void> {
-    const DB_URI = <string>process.env.DB_URI;
+    // const DB_URI = <string>process.env.DB_URI;
+    const DB_URI = `mongodb+srv://dummyuser:dummyuser123@cluster0.ukadaya.mongodb.net/api-rest-ts?retryWrites=true&w=majority`;
     await connect(DB_URI);
 }
 
